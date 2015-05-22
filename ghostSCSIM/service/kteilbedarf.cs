c8e24a3ed478304438391213142d45cfa7fc9985
paca1011 <cpaciner@gmail.com>
2015-05-22 12:42:44 +0200

    Trennung von Domain und Service, Übergabe von KTeilbedarf über Funktionen initiiert

---
+++ ghostSCSIM/service/kteilbedarf.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ghostSCSIM.Domain;
+
+namespace ghostSCSIM.service
+{
+    public class kteilbedarf
+    {
+        public kteilbedarf()
+        {
+
+        }
+
+        //Testkonstruktor zur Übergabe eines Feldes aus einer Domainklasse
+        public Ebedarf ebedarftest = new Ebedarf(3);
+
+        //Testmethode zur Übergabe eines Feldes aus einer Domainklasse
+        public int testmethode(Ebedarf ebedarftest)
+        {
+            int ergebnis = ebedarftest.test;
+
+            return ergebnis;
+        }
+
+        //TODO anstatt leeren Konstruktor befüllten Konstruktor für Zugriff auf ebedarfe verwenden
+        public Ebedarf ebedarf = new Ebedarf();
+
+        public int getk24bedarf(Ebedarf ebedarf)
+        {
+
+            int k24bedarf = ebedarf.e4bedarf + 2 * ebedarf.e29bedarf + 2 * ebedarf.e30bedarf + ebedarf.e31bedarf + 2 * ebedarf.e49bedarf + 2 * ebedarf.e50bedarf + ebedarf.e51bedarf + 2 * ebedarf.e54bedarf + 2 * ebedarf.e55bedarf + ebedarf.e56bedarf;
+
+            return k24bedarf;
+        }
+
+    }
+}
